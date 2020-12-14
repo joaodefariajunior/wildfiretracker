02126ab823e08b0c5e1b8862afdb709c927b6d90
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-12-14 19:57:12 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Map from './component/Map'
+import Loader from './component/Loader'
+import './index.css';
+import  {useState,useEffect} from 'react';
+import Header from './component/Header'
+
+function App() {
+  const [eventData,setEventData]=useState([])
+  const [loading,setLoading]=useState(false)
+
+  useEffect(()=>{
+    const fetchEvents= async()=>{
+      setLoading(true)
+      const res= await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const {events}= await res.json()
+      
+      setEventData(events)
+      setLoading(false)
+
+    }
+    fetchEvents()
+  },[])
+  return (
+    <div>
+      <Header/>
+     {!loading? <Map eventData={eventData}/>:<Loader/>}
+    </div>
+  );
+}
+
+export default App;
